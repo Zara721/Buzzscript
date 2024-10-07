5159f3aa3b324cf14695a9edc86b2239429fcb6d
Zara721 <zaza275@icloud.com>
2024-10-07 12:12:15 -0600

    commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("nav-links").style.right = 0;
+    document.getElementById("close-nav").style.visibility = "visible";
+  }
+
+function closeNav() {
+    document.getElementById("nav-links").style.right = "-35vw";
+    document.getElementById("close-nav").style.visibility = "hidden";
+  }
