c7383f1f84c1d03e5e88234eb752606077757801
Zara721 <zaza275@icloud.com>
2024-11-07 11:37:50 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
   const navigation = document.getElementById("side-nav");
   const nav_button = document.getElementById("menu-btn");
   const overlay = document.getElementById("overlay")
   const menu = document.getElementById("popup-menu");
 
-  nav_button.addEventListener("click", function() {
+  nav_button.addEventListener("click", () => {
     navigation.classList.toggle("show");
     overlay.classList.toggle("show");
   });
 
-  document.querySelector("nav .close-btn").addEventListener("click", function() {
+  document.querySelector("nav .close-btn").addEventListener("click", () => {
     navigation.classList.toggle("show");
+    overlay.classList.toggle("show");
   });
 
   document.onclick = function(e){
@@ -20,11 +21,13 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   } 
 
-  document.getElementById("quiz-btn").addEventListener("click", function() {
+  // add a toggleNav funtion and use that as the callback function
+
+  document.getElementById("quiz-btn").addEventListener("click", () => {
     menu.style.height = "100%";
   });
 
-  document.querySelector("#popup-menu .close-btn").addEventListener("click", function() {
+  document.querySelector("#popup-menu .close-btn").addEventListener("click", () => {
     menu.style.height = "0%";
   });
 
