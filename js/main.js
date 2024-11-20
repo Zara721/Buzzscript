bd681a3c116b4c11b2a6c439384f62a3ae2bac4f
Zara721 <zaza275@icloud.com>
2024-11-20 13:54:00 -0700

    quiz stiff and ducks

--- js/main.js
+++ js/main.js
@@ -2,33 +2,34 @@ document.addEventListener("DOMContentLoaded", () => {
   const navigation = document.getElementById("side-nav");
   const overlay = document.getElementById("overlay")
   const nav_button = document.getElementById("menu-btn");
+  const nav_close_btn = document.querySelector("nav .close-btn");
 
-  // add a toggleNav funtion and use that as the callback function
-  nav_button.addEventListener("click", () => {
+  nav_button.addEventListener("click", toggleNav);
+
+  nav_close_btn.addEventListener("click", toggleNav);
+
+  function toggleNav() {
     navigation.classList.toggle("show");
     overlay.classList.toggle("show");
+  }
+
+  const menu = document.getElementById("popup-menu");
+  const quiz_btn = document.getElementById("quiz-btn");
+  const quiz_close_btn = document.querySelector("#popup-menu .close-btn");
+
+  quiz_btn.addEventListener("click", () => {
+    menu.classList.toggle("show");
   });
 
-  document.querySelector("nav .close-btn").addEventListener("click", () => {
-    navigation.classList.toggle("show");
-    overlay.classList.toggle("show");
+  quiz_close_btn.addEventListener("click", () => {
+    menu.classList.toggle("show");
   });
 
   document.onclick = (e) => {
     if (!navigation.contains(e.target) && !nav_button.contains(e.target) && navigation.classList.contains("show")) {
-        navigation.classList.toggle("show");
-        overlay.classList.toggle("show");
-    }
-  } 
-
-  const menu = document.getElementById("popup-menu");
-  document.getElementById("quiz-btn").addEventListener("click", () => {
-    menu.classList.toggle("show");
-  });
-
-  document.querySelector("#popup-menu .close-btn").addEventListener("click", () => {
-    menu.classList.toggle("show");
-  });
+      toggleNav()
+    } 
+  }
 
 }) 
 
--- js/quiz.js
+++ js/quiz.js
@@ -10,53 +10,76 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(`dom wormking`);
     //load data from the JSON file
     const questions_json = JSON.parse(questions);
-    const horror_questions = questions_json.horror_character
+    let currentQuiz = questions_json.horror_character
+    let currentQuestionIndex = 0;
+    const outcomes = Object.keys(currentQuiz.outcomes);
+    const outcome_dict = {};
+    outcomes.forEach(key => {
+        outcome_dict[key] = 0;
+    });
 
-    for (let q of horror_questions) {
-            console.log(q.question)
-        }
-    
-        let currentQuestionIndex = 0;
-        loadQuestion();
+    console.log(outcome_dict)
+
+    loadQuestion();
 
     function loadQuestion(){
-        const currentQuestion = horror_questions[currentQuestionIndex];
+        const currentQuestion = currentQuiz.questions[currentQuestionIndex];
         document.getElementById(`question`).textContent = currentQuestion.question;
-                // Update the option labels
+        
+        // Update the option labels
         document.querySelector("label[for='option_a']").textContent = currentQuestion.option_a;
         document.querySelector("label[for='option_b']").textContent = currentQuestion.option_b;
         document.querySelector("label[for='option_c']").textContent = currentQuestion.option_c;
         document.querySelector("label[for='option_d']").textContent = currentQuestion.option_d;
     }
 
-        //document.querySelector(``)
-        // Event listener for form submission (answering the question)
-        document.getElementById("choices").addEventListener("submit", function(event) {
-            event.preventDefault();  // Prevent form submission from refreshing the page
-    
-            // Get the selected option
-            const selectedOption = document.querySelector('input[name="choice"]:checked');
-            
-            if (selectedOption) {
-                const selectedAnswer = selectedOption.nextElementSibling.textContent;
-                console.log("You selected: " + selectedAnswer);
-                
-                // Move to the next question
-                currentQuestionIndex++;
-    
-                // If there are more questions, load the next one
-                if (currentQuestionIndex < horror_questions.length) {
-                    loadQuestion();
+    function updateOutcome(chosen_outcome) {
+        // Update the outcome dictionary based on the parsed outcomes
+        let outcome_array = currentQuiz.questions[currentQuestionIndex][chosen_outcome];
 
-                } else {
-
-                    document.getElementById(`question`).textContent = `Thanks for playing`;
-                    document.getElementById(`choices`).style.display = `none`;
-                }
-            } else {
-                alert("Please select an option.");
+        outcome_array.forEach(([outcome, points]) => {
+            if (outcome in outcome_dict) {
+                outcome_dict[outcome] += parseInt(points);
             }
         });
+        console.log(outcome_dict)
+    }
+
+    //document.querySelector(``)
+    // Event listener for form submission (answering the question)
+    document.getElementById("choices").addEventListener("submit", function(event) {
+        event.preventDefault();  // Prevent form submission from refreshing the page
+
+        // Get the selected option
+        const selectedOption = document.querySelector('input[name="choice"]:checked');
+        chosen_outcome = selectedOption.id.replace("option", "outcome")
+        console.log(chosen_outcome)
+        // console.log(currentQuiz.questions[currentQuestionIndex][chosen_outcome])
+
+        updateOutcome(chosen_outcome)
+        
+        if (selectedOption) {
+            const selectedAnswer = selectedOption.nextElementSibling.textContent;
+            console.log("You selected: " + selectedAnswer);
+            
+            // Move to the next question
+            currentQuestionIndex++;
+
+            // If there are more questions, load the next one
+            if (currentQuestionIndex < currentQuiz.questions.length) {
+                loadQuestion();
+
+            } else {
+                let users_outcome = Object.keys(outcome_dict).reduce((a, b) => outcome_dict[a] > outcome_dict[b] ? a : b);
+                users_outcome = currentQuiz.outcomes[users_outcome]
+
+                document.getElementById(`question`).textContent = `Thanks for playing. \n You got ${users_outcome}.`;
+                document.getElementById(`choices`).style.display = `none`;
+            }
+        } else {
+            alert("Please select an option.");
+        }
+      });
 
     })
 
